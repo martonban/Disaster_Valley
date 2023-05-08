47a5425d010f18b60e6767386783a6f0fdff3ea6
Márton Bán <30813844+martonban@users.noreply.github.com>
2023-05-08 17:07:23 +0200

    Hot Fix Delta Time

--- src/main/java/engine/Window.java
+++ src/main/java/engine/Window.java
@@ -3,7 +3,6 @@ package engine;
 import org.lwjgl.Version;
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.opengl.GL;
-import util.Time;
 
 import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
 import static org.lwjgl.glfw.GLFW.*;
@@ -108,8 +107,8 @@ public class Window {
     }
 
     public void loop(){
-        float beginTime = Time.getTime();
-        float endTime = Time.getTime();
+        float beginTime = (float)glfwGetTime();
+        float endTime;
         float dt = -1.0f;
 
         while(!glfwWindowShouldClose(glfwWindow)) {
@@ -124,7 +123,7 @@ public class Window {
 
             glfwSwapBuffers(glfwWindow);
 
-            endTime = Time.getTime();
+            endTime = (float)glfwGetTime();
             dt = endTime - beginTime;
             beginTime = endTime;
         }

0492f0bdd49c1ce2bfa19a72bb97a4e83ea1bfe0
Márton Bán <30813844+martonban@users.noreply.github.com>
2023-06-11 16:28:39 +0200

    ColliosionBoxRenderer wip

--- src/main/java/renderer/LineRendererDataHandler.java
+++ src/main/java/renderer/LineRendererDataHandler.java
@@ -1,4 +1,52 @@
 package renderer;
 
-public interface LineRendererDataHandler {
+import java.util.ArrayList;
+
+public class LineRendererDataHandler {
+    private ArrayList <Float> points = new ArrayList<>();
+    private static ArrayList <Integer> indices = new ArrayList<>();
+
+    private LineRender lineRender;
+
+
+
+    public void render() {
+        lineRender.render();
+    }
+
+    // TO-DO
+    public void changePoint(){}
+
+    public void addNewPointsToTheRenderer(float newPoints[]) {
+        for(float i : newPoints) {
+            this.points.add(i);
+        }
+    }
+
+    public void addNewIndicesToTheRenderer(int newIndices[]) {
+        for(int i : newIndices) {
+            this.indices.add(i);
+        }
+    }
+
+    public static int requestIndices() {
+        return indices.get(indices.size());
+    }
+
+    public float[] getPointsData() {
+        float[] pointsBuffer = new float[this.points.size()];
+        for(int i = 0; i < this.points.size(); i++ ) {
+            pointsBuffer[i] = points.get(i);
+        }
+        return pointsBuffer;
+    }
+
+    public int[] getIndicesData() {
+        int[] indicesBuffer = new int[this.indices.size()];
+        for(int i = 0; i < this.indices.size(); i++ ) {
+            indicesBuffer[i] = indices.get(i);
+        }
+        return indicesBuffer;
+    }
+
 }
--- src/main/java/renderer/component/CollisionBoxGraphics.java
+++ src/main/java/renderer/component/CollisionBoxGraphics.java
@@ -1,15 +1,83 @@
 package renderer.component;
 
 import org.joml.Vector2f;
-import org.joml.Vector2i;
+import org.joml.Vector4f;
+import renderer.LineRendererDataHandler;
 
 public class CollisionBoxGraphics {
     private float[] points;
     private int[] indices;
 
-    public CollisionBoxGraphics(Vector2f position, Vector2i indices) {
-        this.points = generatePoints(position);
-        this.indices = generateIndices(indices);
+    public CollisionBoxGraphics(Vector2f position, Vector2f size) {
+        this.points = generatePoints(position, size);
+        this.indices = generateIndices();
+    }
+
+
+
+
+
+
+
+
+    /*
+    *  3-----------4
+    *  |           |
+    *  |           |
+    *  |           |
+    *  1-----------2
+    *
+    * */
+    public int[] generateIndices() {
+        int[] buffer = new int[10];
+
+        buffer[0] = LineRendererDataHandler.requestIndices();
+        buffer[1] = buffer[0] + 1;
+        buffer[2] = buffer[0];
+        buffer[3] = buffer[0] + 2;
+        buffer[4] = buffer[0] + 2;
+        buffer[5] = buffer[0] + 3;
+        buffer[6] = buffer[0] + 3;
+        buffer[7] = buffer[0] + 1;
+
+        return buffer;
+    }
+
+    public float[] generatePoints (Vector2f pos, Vector2f size) {
+        float[] buffer = new float[4 * 6];
+        Vector4f color = new Vector4f (1.0f, 0.0f, 0.0f, 1.0f);
+        Vector2f rightBottomPoint = new Vector2f(pos.x + size.x, pos.y);
+        Vector2f leftUpPoint = new Vector2f(pos.x, pos.y + size.y);
+        Vector2f rightUpPoint = new Vector2f(pos.x + size.x, pos.y + size.y);
+        buffer[0] = pos.x;
+        buffer[1] = pos.y;
+        buffer[2] = color.x;
+        buffer[3] = color.y;
+        buffer[4] = color.z;
+        buffer[5] = color.w;
+
+        buffer[6] = rightBottomPoint.x;
+        buffer[7] = rightBottomPoint.y;
+        buffer[8] = color.x;
+        buffer[9] = color.y;
+        buffer[10] = color.z;
+        buffer[11] = color.w;
+
+        buffer[12] = leftUpPoint.x;
+        buffer[13] = leftUpPoint.y;
+        buffer[14] = color.x;
+        buffer[15] = color.y;
+        buffer[16] = color.z;
+        buffer[17] = color.w;
+
+        buffer[18] = rightUpPoint.x;
+        buffer[19] = rightUpPoint.y;
+        buffer[20] = color.x;
+        buffer[21] = color.y;
+        buffer[22] = color.z;
+        buffer[23] = color.w;
+
+        return buffer;
     }
 
 
